bf9cd6b6b78c773493ca738d7ae923f4f7d8c807
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-02 16:39:12 +0530

    stringlen.C

---
+++ STRING LENGTH.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char name[200];
+int length;
+printf("enter the length of the string");
+scanf("%d",&length);
+length=strlen(name);
+printf("\n the length of the string is",length);
+}
