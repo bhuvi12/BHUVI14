4eafaf002bf222b7bac7606285f1b82a719dab2d
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-05 16:56:36 +0530

    word.C

---
+++ word.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[i];
+int i,count=0;
+printf("\n enter the string");
+scanf("%s",&a[i]);
+for(i=0;i<20;i++)
+{
+if(a[i]='  ')
+{
+count++;
+}
+}
+printf("\n the result is",count);
+}
