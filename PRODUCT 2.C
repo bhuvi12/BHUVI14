7047854eb1b99af6bca6461d014dbb3f845916a5
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-09 16:55:30 +0530

    product 2.C

---
+++ PRODUCT 2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n enter the value of a and b");
+scanf("%d",&a,&b);
+c=a*b;
+if(c%2==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
